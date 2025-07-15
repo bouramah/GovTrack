da699db1ae05ce2a19f2f18521b180ec2832b912
bouramah <douty38oulare@gmail.com>
2025-07-15 14:44:34 +0000

    Suppression Taskora

--- govtrack-frontend/lib/api.ts
+++ govtrack-frontend/lib/api.ts
@@ -618,7 +618,7 @@ class ApiClient {
 
   constructor() {
     this.client = axios.create({
-      baseURL: API_BASE_URL,
+      baseURL: DEV__API_BASE_URL,
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
